a5965c7f6802043a44404d485cec2a99c76631df
Andres Martino Camacho <andrelomartino@gmail.com>
2022-01-27 13:16:10 -0300

    ej10

---
+++ Ejercicio10/src/Ejercicio10/Main.java
@@ -0,0 +1,12 @@
+package Ejercicio10;
+public class Main {
+
+    public static void main(String[] args) {
+        Arreglos arreglo=new Arreglos();
+        arreglo.llenarV1();
+        arreglo.ordenarV1();
+        arreglo.llenarV2();
+        arreglo.mostrandoVectores();
+    }
+    
+}
