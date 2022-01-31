7b876eb94ddfa15ea5ca31d6c4972df7a01dad0d
Andres Martino Camacho <andrelomartino@gmail.com>
2022-01-31 12:01:09 -0300

    extra4

---
+++ Extra4/src/Extra4/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Extra4;
+
+/**
+ *
+ * @author Andrelo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Nif nif=new Nif();
+        nif.crearNif();
+        nif.mostrarNif();
+        
+    }
+    
+}
