02a4e556db2df77c9eaa66928520d3c9ec25fd34
Andres Martino Camacho <andrelomartino@gmail.com>
2022-01-31 11:21:25 -0300

    extra3

---
+++ Extra3/src/Extra3/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Extra3;
+
+/**
+ *
+ * @author Andrelo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Raices raices=new Raices(1,4,4);
+       raices.getDiscriminante();
+       raices.calcular();
+    }
+    
+}
